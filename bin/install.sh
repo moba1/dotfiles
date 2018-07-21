24ca37a9c2ebe444c9433e4d3aea52c82a437d3f
moba1 <idea.care.origin@gmail.com>
2018-07-21 18:21:49 +0900

    add: add debian distribution

--- bin/install.sh
+++ bin/install.sh
@@ -5,10 +5,25 @@
 export PATH="~/.homebrew/bin:${PATH}"
 export PATH="~/.cargo/bin:${PATH}"
 
+get_linux_dist() {
+    dist="unsupported dist"
+
+    if [ -e /etc/debian_version ]
+    then
+        dist="debian"
+    fi
+
+    echo $dist
+}
+
 case "$(uname)" in
     "Darwin")
         ./mitamae local roles/darwin.rb
         ;;
+    "linux")
+        sudo ./mitamae local roles/"$(get_linux_dist)".rb
+        ;;
 esac
 
 rm "mitamae"
+
