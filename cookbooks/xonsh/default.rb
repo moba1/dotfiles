c370273dc6d7960ee6a8fca56cb7c5cffb4ed7cc
moba1 <idea.care.origin@gmail.com>
2018-07-23 04:04:36 +0900

    add: add 'exec xonsh' for ubuntu

--- cookbooks/xonsh/default.rb
+++ cookbooks/xonsh/default.rb
@@ -10,4 +10,9 @@ when "darwin"
     execute "echo \'exec ~/.homebrew/bin/xonsh\' >> ~/.bash_profile" do
         not_if "grep \"~/.homebrew/bin/xonsh\" ~/.bash_profile"
     end
+when "ubuntu"
+    bash_profile_path = "#{ENV["HOME"]}/.bash_profile"
+    execute "echo 'exec xonsh' >> #{bash_profile_path}" do
+        not_if "grep 'exex xonsh' #{bash_profile_path}"
+    end
 end
