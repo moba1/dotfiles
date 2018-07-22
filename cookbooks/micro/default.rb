6a9d342298a5aab162f059a860d1ae34ac2dbf5f
moba1 <idea.care.origin@gmail.com>
2018-07-23 01:47:02 +0900

    fix: fix linux permission

--- cookbooks/micro/default.rb
+++ cookbooks/micro/default.rb
@@ -3,11 +3,15 @@ when "darwin"
     package "micro"
 else
     binary_dir = "~/.local/bin"
+    group_id = 1000
     directory File.expand_path(binary_dir) do
         not_if "[ -e #{binary_dir} ]"
+        mode "755"
+        owner ENV["USER"]
+        group group_id
     end
     repository = "https://getmic.ro"
-    execute "curl #{repository} | bash && mv micro #{binary_dir}" do
-        not_if "[ -e #{binary_dir} ]"
+    execute "curl #{repository} | bash && chown #{ENV["USER"]}:#{group_id} micro && mv micro #{binary_dir}" do
+        not_if "[ -e #{binary_dir}/micro ]"
     end
 end
