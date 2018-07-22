73bd9700f113d0937492078035590a1dc75eabe3
moba1 <idea.care.origin@gmail.com>
2018-07-23 03:23:41 +0900

    fix: fix apt source and update

--- cookbooks/apt/default.rb
+++ cookbooks/apt/default.rb
@@ -1,6 +1,8 @@
-execute "apt update" do
-    only_if "[ \"$(apt list --upgradable 2>/dev/null | wc -l)\" -gt 1 ]"
+execute "sed -i -e \"s%archive.ubuntu.com/ubuntu/%ftp.iij.ad.jp/pub/linux/ubuntu/archive/%g\" /etc/apt/sources.list" do
+    not_if "[ grep ftp.iij.ad.jp /etc/apt/sources.list ]"
 end
+
+execute "apt update"
 
 execute "apt upgrade -y" do
     only_if "[ \"$(apt list --upgradable 2>/dev/null | wc -l)\" -gt 1 ]"
