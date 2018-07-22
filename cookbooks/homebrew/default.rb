0b812de8d9c89a335cc277aa8e99e6ad7e86e502
moba1 <idea.care.origin@gmail.com>
2018-07-23 03:06:56 +0900

    fix: fix homebrew prefix

--- cookbooks/homebrew/default.rb
+++ cookbooks/homebrew/default.rb
@@ -1,11 +1,14 @@
-homebrew_prefix = "~/.homebrew"
+homebrew_prefix = File.expand_path("~/.homebrew")
 repository = "https://github.com/Homebrew/homebrew/tarball/master"
-
 brew = "#{homebrew_prefix}/bin/brew"
-execute "mkdir -p #{homebrew_prefix} && curl -L #{repository} | tar xz --strip 1 -C #{homebrew_prefix}" do
+
+directory homebrew_prefix
+
+execute "curl -L #{repository} | tar xz --strip 1 -C #{homebrew_prefix}" do
     not_if "[[ -e #{brew} ]]"
 end
 
 execute "#{brew} update" do
     only_if "brew install 2>&1 | grep -i \"error: unknown command\""
 end
+
