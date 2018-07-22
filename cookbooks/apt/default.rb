08746ebf540aa18e88e009cbe095a9aa2cfeb97d
moba1 <idea.care.origin@gmail.com>
2018-07-23 03:02:27 +0900

    fix: apt

--- cookbooks/apt/default.rb
+++ cookbooks/apt/default.rb
@@ -1,3 +1,8 @@
 execute "apt update" do
-    only_if "[ -n \"$(apt list --upgradeable | grep -v \"Listing... Donw\")\" ]"
+    only_if "[ \"$(apt list --upgradable 2>/dev/null | wc -l)\" -gt 1 ]"
 end
+
+execute "apt upgrade -y" do
+    only_if "[ \"$(apt list --upgradable 2>/dev/null | wc -l)\" -gt 1 ]"
+end
+
