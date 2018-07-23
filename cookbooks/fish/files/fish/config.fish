4a9449ab908dd0d6efda2af22d3302e60189cbfa
moba1 <idea.care.origin@gmail.com>
2018-07-23 16:37:03 +0900

    fix: fish git prompt

--- cookbooks/fish/files/fish/config.fish
+++ cookbooks/fish/files/fish/config.fish
@@ -10,6 +10,24 @@ end
 
 set -gx PYTHONUSERBASE ~/.local
 
+# for git
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showupstream 'yes'
 set __fish_git_prompt_color_branch cyan
 set __fish_git_prompt_color_upstream_ahead green
 set __fish_git_prompt_color_upstream_behind magenta
+set __fish_git_prompt_show_informative_status 'yes'
+## status characters
+set __fish_git_prompt_char_upstream_prefix '.'
+set __fish_git_prompt_char_upstream_ahead 'A'
+set __fish_git_prompt_char_upstream_behind 'B'
+set __fish_git_prompt_char_stateseparator ':'
+set __fish_git_prompt_char_dirtystate 'm'
+set __fish_git_prompt_char_invalidstate 'x'
+set __fish_git_prompt_char_stagedstate 'M'
+set __fish_git_prompt_char_untrackedfiles 'U'
+set __fish_git_prompt_char_cleanstate 'clean'
+set __fish_git_prompt_char_stashstate '^'
+set __fish_git_prompt_describe_style 'branch'
