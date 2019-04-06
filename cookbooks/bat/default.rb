94327c267af7ee96d131c67e4aef442bd45aaf56
moba1 <idea.care.origin@gmail.com>
2019-04-07 02:34:03 +0900

    batのインストール時に, --forceオプションをつけるようにした(つけないと羃等性が保たれない)

--- cookbooks/bat/default.rb
+++ cookbooks/bat/default.rb
@@ -1,6 +1,6 @@
 include_recipe "../rust/default"
 
 execute "install bat" do
-  command "#{ENV['HOME']}/.cargo/bin/cargo install bat"
+  command "#{ENV['HOME']}/.cargo/bin/cargo install --force bat"
   user node[:username]
 end
