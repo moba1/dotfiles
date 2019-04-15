40ab000862e7b5113ec088eeec3178a416ea74eb
moba1 <idea.care.origin@gmail.com>
2019-04-16 00:54:30 +0900

    set color

--- cookbooks/fish/files/fish/config.fish
+++ cookbooks/fish/files/fish/config.fish
@@ -30,3 +30,13 @@ set -l CUSTOM_FISH_FILE "~/.config/fish/custom.fish"
 if ls $CUSTOM_FISH_FILE >/dev/null 2>&1
   source $CUSTOM_FISH_FILE
 end
+
+# set color
+set fish_color_error red --bold
+set fish_color_param white
+set fish_color_command cyan --bold
+set fish_color_autosuggestion green
+set fish_color_operator bryellow --bold
+set fish_color_comment green --bold
+set fish_color_search_match --background=magenta
+set fish_color_escape yellow --bold
