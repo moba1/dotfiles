21000c95a3b15c383f0887e53688ff8103cc0a71
moba1 <idea.care.origin@gmail.com>
2019-05-01 19:08:33 +0900

    right_promptを消して、promptにgitを埋め込むようにした

--- cookbooks/fish/files/fish/functions/fish_prompt.fish
+++ cookbooks/fish/files/fish/functions/fish_prompt.fish
@@ -1,6 +1,7 @@
 function fish_prompt --description "left prompt"
     set -l exit_code $status
+    set -l git_prompt (__fish_git_prompt)
 
-    printf "[ \e[1;32m%s\e[0m ]\n" (prompt_pwd)
+    printf "[ \e[1;32m%s\e[0m%s ]\n" (prompt_pwd) $git_prompt
     printf "\e[1;93m$USER@%s\e[0m%s > " (hostname | cut -d . -f 1) ([ $exit_code -ne 0 ]; and echo -e " \e[1;31m!!!\e[0m ")
 end
--- cookbooks/fish/files/fish/functions/fish_right_prompt.fish
+++
@@ -1,4 +0,0 @@
-function fish_right_prompt --description "fish right prompt"
-    #printf "\e[1;36m%s\e[0m" (__fish_git_prompt)
-    __fish_git_prompt
-end
