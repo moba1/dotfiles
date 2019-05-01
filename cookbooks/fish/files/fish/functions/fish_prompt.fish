0f52886da4ddf50e0eb7503528082cc4627e02f4
moba1 <idea.care.origin@gmail.com>
2019-05-01 19:15:53 +0900

    プロンプトにおさかなさんを入れた

--- cookbooks/fish/files/fish/functions/fish_prompt.fish
+++ cookbooks/fish/files/fish/functions/fish_prompt.fish
@@ -3,5 +3,5 @@ function fish_prompt --description "left prompt"
     set -l git_prompt (__fish_git_prompt)
 
     printf "[ \e[1;32m%s\e[0m%s ]\n" (prompt_pwd) $git_prompt
-    printf "\e[1;93m$USER@%s\e[0m%s > " (hostname | cut -d . -f 1) ([ $exit_code -ne 0 ]; and echo -e " \e[1;31m!!!\e[0m ")
+    printf "🎣 "
 end
