63ca1ef26a61636bfe0c2a57f905f9c645f8d9bb
moba1 <idea.care.origin@gmail.com>
2019-07-24 11:36:13 +0900

    adjust prompt colors & add hostname to prompt

--- cookbooks/fish/files/fish/functions/fish_prompt.fish
+++ cookbooks/fish/files/fish/functions/fish_prompt.fish
@@ -1,10 +1,10 @@
 function fish_prompt --description "left prompt"
     set -l exit_code $status
 
-    printf "[ \e[1;32m%s\e[0m%s ]\n" (prompt_pwd) (__fish_git_prompt)
+    printf "[ \e[36mm%s\e[0m@\e[1;32m%s\e[0m%s ]\n" (hostname) (prompt_pwd) (__fish_git_prompt)
     if [ $exit_code -eq 0 ]
-      printf "\e[1;36m>< <(('>\e[0m .oO "
+      printf "\e[36m>< <(('>\e[0m .oO "
     else
-      printf "\e[1;35m>< <((->\e[0m .oO "
+      printf "\e[35m>< <((->\e[0m .oO "
     end
 end
