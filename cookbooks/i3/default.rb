b5e28a80f2acdfb372fc54115445314a670860f4
moba1 <idea.care.origin@gmail.com>
2019-09-10 13:14:15 +0900

    i3と一緒にalsaを入れるようにした

--- cookbooks/i3/default.rb
+++ cookbooks/i3/default.rb
@@ -9,18 +9,23 @@ case node[:platform]
 when 'debian', 'ubuntu', 'mint'
   package "fonts-font-awesome"
   package "fonts-hack-ttf"
+  package "alsa-utils"
 when 'arch'
   package "ttf-font-awesome"
   package "ttf-hack"
+  package "alsa-tools"
 when 'redhat', 'fedora'
   package "fontawesome-fonts-web"
   package "hack-fonts"
+  package "alsa-utils"
 when 'opensuse'
   package "fontawesome-fonts"
   package "hack-fonts"
+  package "alsa-tools"
 when 'gentoo'
   package "media-fonts/fontawesome"
   package "media-fonts/hack"
+  package "media-sound/alsa-tools"
 end
 
 config_dir = File.join(node[:home], ".config")
