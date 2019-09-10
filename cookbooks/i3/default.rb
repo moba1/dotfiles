4dedc5cad6972fd820eb36b04812e1661cbe88ec
moba1 <idea.care.origin@gmail.com>
2019-09-10 13:20:18 +0900

    alsaの変わりにpulseaudioを利用するように変更

--- cookbooks/i3/default.rb
+++ cookbooks/i3/default.rb
@@ -4,28 +4,25 @@ package "dmenu"
 package "gnome-screensaver"
 package "gdm"
 package "sysstat"
+package "pulseaudio"
 
 case node[:platform]
 when 'debian', 'ubuntu', 'mint'
   package "fonts-font-awesome"
   package "fonts-hack-ttf"
-  package "alsa-utils"
+  package "pulseaudio-utils"
 when 'arch'
   package "ttf-font-awesome"
   package "ttf-hack"
-  package "alsa-tools"
 when 'redhat', 'fedora'
   package "fontawesome-fonts-web"
   package "hack-fonts"
-  package "alsa-utils"
 when 'opensuse'
   package "fontawesome-fonts"
   package "hack-fonts"
-  package "alsa-tools"
 when 'gentoo'
   package "media-fonts/fontawesome"
   package "media-fonts/hack"
-  package "media-sound/alsa-tools"
 end
 
 config_dir = File.join(node[:home], ".config")
