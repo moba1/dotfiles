a8f87d591696c4c36617408de848858155887a5d
moba1 <moba2@protonmai.com>
2019-09-12 00:14:22 +0900

    ストレージを自動でとるようにした

--- cookbooks/i3/files/i3blocks/scripts/disk.py
+++ cookbooks/i3/files/i3blocks/scripts/disk.py
@@ -50,6 +50,20 @@ class ByteSize(object):
             exponent = 8
         return self.__byte / (1024**exponent)
 
+class DiskType(enum.Enum):
+    SERIAL = "\uf0a0"
+    MULTI_MEDIA_CARD = "\uf7c2"
+    UNKNOWN = "\uf128"
+
+    @classmethod
+    def guess_disk_type(cls, partition):
+        if re.search(r'^/dev/sd.?([1-9][0-9]*)?$', partition):
+            return cls.SERIAL
+        elif re.search(r'/dev/mmcblk[0-9]+p[0-9]+$', partition):
+            return cls.MULTI_MEDIA_CARD
+
+        return cls.UNKNOWN
+
 class Disk(object):
     '''
     ディスクの状態を保持する
@@ -62,40 +76,52 @@ class Disk(object):
     '''
     def __init__(self, partition, byte_unit = ByteUnit.GIGA_BYTE):
         # dfでディスクの使用量と利用可能量を取得
-        command = f"df --output=avail --output=used --block-size=1 /dev/{partition} | tail -1"
+        command = f"df --output=avail --output=used --block-size=1 {partition} | tail -1"
         output = subprocess.check_output(command, shell=True).decode('UTF-8').rstrip()
 
-        self.__available, self.__used = map(ByteSize, map(int, output.split(' ')))
+        self.__available, self.__used = map(
+            ByteSize,
+            map(int, output.strip().split(' '))
+        )
         self.__byte_unit = byte_unit
         self.__partition = partition
 
     def __repr__(self):
+        disk_type = DiskType.guess_disk_type(self.__partition)
+        disk = f"{disk_type.value} {self.__partition.split('/')[-1]}"
+        if disk_type == DiskType.UNKNOWN:
+            return f"{disk} <b><span color='#dc322f'>unknown disk type</span></b>"
+
+        if self.__available.byte() == 0:
+            return f"{disk} <b><span color='#dc322f'>no space</span></b>"
+
         # ディスクの開き率を計算
-        used_rate = (self.__available.byte() - self.__used.byte()) * 100 / self.__available.byte()
+        available_rate = (self.__available.byte() - self.__used.byte()) * 100 / self.__available.byte()
 
         available, used = self.__available.byte(self.__byte_unit), self.__used.byte(self.__byte_unit)
-        raw_disk_usage_message = "{:.2f} / {:.2f} [{}]".format(used, available, self.__byte_unit.value)
+        raw_disk_usage_message = "{:.2f} [{}]".format(used, self.__byte_unit.value)
         # ディスクの使用量に応じて色変える
-        if used_rate <= 10:
+        if available_rate <= 10:
             # 10%以下しか利用できない状態は危険なので、赤色に
             raw_disk_usage_message = f"<b><span color='#dc322f'>{raw_disk_usage_message}</span></b>"
-        elif used_rate <= 20:
+        elif available_rate <= 20:
             # 20%以下か利用できない場合は警告の意味を込めてオレンジ色に
             raw_disk_usage_message = f"<b><span color='#cb4b16'>{raw_disk_usage_message}</span></b>"
 
-        # ディスクの種別に応じてアイコンを変える
-        if re.search(r'^sd.[1-9][0-9]*', self.__partition):
-            # USBやHDD、SSDなどのSATA接続された機器
-            return f"\uf0a0 {raw_disk_usage_message}"
-        elif re.search(r'mmcblk[0-9]+p[0-9]+', self.__partition):
-            # マルチメディアカード(SDカード)の場合
-            return f"\uf7c2 {raw_disk_usage_message}"
-        else:
-            # それ以外は現状未サポートとする
-            return '<b><span color=#dc322f">unknown disk type</span></b>'
+        return f"{disk} {raw_disk_usage_message}"
+
+def get_disks():
+    command = "df | awk '$6 != \"/boot\"' | awk 'match($1,\"/dev/\") != 0' | awk '{print $1}'"
+    output = subprocess.check_output(command, shell=True).decode('UTF-8').rstrip()
+
+    return output.split("\n")
 
 def main():
-    print(Disk('sda2'))
+    print(
+        ' '.join(
+            map(lambda partition: str(Disk(partition)), get_disks())
+        )
+    )
 
 main()
 
