a6605ec98344981280339e5ca4c3ada00308fe75
moba1 <idea.care.origin@gmail.com>
2019-09-12 13:10:25 +0900

    Rustのcrate.ioから引っ張ってくるパッケージはcookbooks/rustにまとめた

--- cookbooks/rust/default.rb
+++ cookbooks/rust/default.rb
@@ -1,8 +1,22 @@
-rustup_exe = File.join(node[:home], '.cargo/bin/rustup')
-if not File.exist?(rustup_exe)
-    execute "rustup setup" do
-        command "sh -c 'curl https://sh.rustup.rs -sSf' | sh -s -- -y"
-        user node[:username]
-    end
+rustup_exe = File.join(node[:home], '.cargo', 'bin', 'rustup')
+execute "rustup setup" do
+  command "sh -c 'curl https://sh.rustup.rs -sSf' | sh -s -- -y"
+  user node[:username]
+  not_if "stat #{rustup_exe}"
+end
+
+cargo_bin = File.join(node[:home], ".cargo", "bin")
+for pack in ["bat", "hexyl", "viu"]
+  execute "install #{pack}" do
+    command "#{File.join(cargo_bin, "cargo")} install --force #{pack}"
+    user node[:username]
+    not_if "stat #{File.join(cargo_bin, pack)}"
+  end
+end
+
+execute "install cargo-update" do
+  command "#{File.join(cargo_bin, "cargo")} install --force cargo-update"
+  user node[:username]
+  not_if "cargo install-update --all"
 end
 
