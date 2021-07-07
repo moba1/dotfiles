2e057e152fd1548e5985a082f46ac92297ff37fe
moba1 <moba2@protonmail.com>
2021-07-08 02:47:24 +0900

    add mac support

--- main.go
+++ main.go
@@ -2,18 +2,34 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 
 	"github.com/moba1/dotfiles/taskset/commonset"
+	"github.com/moba1/dotfiles/taskset/darwinset"
 	"github.com/moba1/dotsetup"
 )
 
 func main() {
 	cs := commonset.Commands()
+	switch dotsetup.Os {
+	case "windows":
+		log.Fatalln("windows not supported")
+	case "darwin":
+		cs = append(darwinset.Commands(), cs...)
+	}
 	s := dotsetup.NewScript(cs)
 
 	flag.BoolVar(&s.Debug, "debug", false, "debug mode")
+	dryRun := flag.Bool("dry-run", false, "dry run")
 	flag.Parse()
+
+	if *dryRun {
+		for _, command := range s.Flat() {
+			fmt.Println(command)
+		}
+		return
+	}
 
 	if err := s.Execute(); err != nil {
 		log.Fatalln(err)
---
+++ taskset/darwinset/homebrew.go
@@ -0,0 +1,15 @@
+package darwinset
+
+import "github.com/moba1/dotsetup"
+
+func homebrew() []dotsetup.Command {
+	return []dotsetup.Command{
+		&dotsetup.Execute{
+			RawCommand: []string{
+				"/bin/bash",
+				"-c",
+				"\"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)\"",
+			},
+		},
+	}
+}
