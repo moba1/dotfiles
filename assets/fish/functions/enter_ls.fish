a02a5adeae92419821356f5d0f3b3e698f21349c
moba1b <moba2@protonmail.com>
2022-01-13 09:51:57 +0900

    add enter_ls

---
+++ assets/fish/functions/enter_ls.fish
@@ -0,0 +1,10 @@
+function enter_ls
+  set -l cmd (commandline)
+  if test -z "$cmd"
+    echo
+    ls
+    # remove `ls`'s new line output
+    printf "\033[1A"
+  end
+  commandline -f execute
+end
---
+++ assets/fish/functions/fish_user_key_bindings.fish
@@ -0,0 +1,3 @@
+function fish_user_key_bindings
+  bind \r enter_ls
+end
