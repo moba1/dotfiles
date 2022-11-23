96bfeb20dceca241d9b75042341c373d434883fd
moba1b <moba2@protonmail.com>
2022-11-24 01:11:40 +0900

    add patched & MIT License statement

--- nixpkgs/packages/kubectx/default.nix
+++ nixpkgs/packages/kubectx/default.nix
@@ -1,3 +1,8 @@
+# Partially referenced from nixpkgs:
+#
+# Copyright (c) 2003-2022 Eelco Dolstra and the Nixpkgs/NixOS contributors
+# Released under the MIT license
+# https://opensource.org/licenses/mit-license.php
 { fetchFromGitHub
 , buildGoModule
 , installShellFiles
@@ -7,11 +12,15 @@ buildGoModule rec {
   pname = "kubectx";
   version = "0.9.4";
 
-  vendorSha256 = "sha256-4sQaqC0BOsDfWH3cHy2EMQNMq6qiAcbV+RwxCdcSxsg=";
+  vendorSha256 = "sha256-p4KUBmJw6hWG1J2qwg4QBbh6Vo1cr/HQz0IqytIDJjU=";
 
   doCheck = false;
 
   ldflags = [ "-s" "-w" "-X main.version=v${version}" ];
+
+  patches = [
+    ./bump-golang-x-sys.patch
+  ];
 
   src = fetchFromGitHub {
     owner = "ahmetb";
