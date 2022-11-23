a3c422324bf56ec0fb20d18be458242c5168753d
moba1 <moba2@protonmail.com>
2022-11-23 23:03:54 +0900

    refactor kubectx

---
+++ nixpkgs/packages/kubectx/default.nix
@@ -0,0 +1,24 @@
+{ lib
+, fetchFromGitHub
+, buildGoModule
+}:
+
+buildGoModule rec {
+  pname = "kubectx";
+  version = "0.9.4";
+
+  vendorSha256 = "sha256-4sQaqC0BOsDfWH3cHy2EMQNMq6qiAcbV+RwxCdcSxsg=";
+
+  doCheck = false;
+
+  ldflags = [ "-s" "-w" "-X main.version=v${version}" ];
+
+  src = fetchFromGitHub {
+    owner = "ahmetb";
+    repo = "kubectx";
+    rev = "v${version}";
+    sha256 = "WY0zFt76mvdzk/s2Rzqys8n+DVw6qg7V6Y8JncOUVCM=";
+  };
+
+  subPackages = [ "cmd/kubectx" "cmd/kubens" ];
+}
