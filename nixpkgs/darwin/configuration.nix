4ae0298dd4d5ee5aa16cc45c8df56131dd93cd73
moba1b <moba2@protonmail.com>
2022-11-24 16:21:23 +0900

    refactor darwin config

--- nixpkgs/darwin/configuration.nix
+++ nixpkgs/darwin/configuration.nix
@@ -1,12 +1,10 @@
 { pkgs, ... }:
 {
-  imports = [ <home-manager/nix-darwin> ];
+  imports = [ <home-manager/nix-darwin> ~/.nixpkgs/darwin-configuration.nix ];
 
   # set login shell configuration
   programs.fish.enable = true;
-  services.nix-daemon.enable = true;
   home-manager.useGlobalPkgs = true;
-  system.stateVersion = 4;
 
   environment.systemPackages =
     [ pkgs.gnupg
