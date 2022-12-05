4303393059878bf43766ed18b45b7f784f846cf2
moba1 <moba2@protonmail.com>
2022-12-06 01:15:05 +0900

    add EDITOR env

--- nixpkgs/home.base.nix
+++ nixpkgs/home.base.nix
@@ -235,6 +235,8 @@ in
       function preexec --on-event fish_preexec
         printf "\e[1mexecute time: \e[4;33m%s\e[0m\n" (date '+%F (%a) %T')
       end
+
+      set -x EDITOR vim
     '';
   };
 }
