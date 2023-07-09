c3e369d214c6713d9330824247a1eab69f46578d
moba1 <moba2@protonmail.com>
2023-07-09 22:28:32 +0900

    fix prompt

--- nixpkgs/home.base.nix
+++ nixpkgs/home.base.nix
@@ -61,7 +61,7 @@ in
     enable = true;
     settings = {
       character = {
-        success_symbol = "[ ](bold blue)";
+        success_symbol = "[󰈺 ](bold blue)";
         error_symbol = "[ ](bold red)";
       };
       package.disabled = true;
