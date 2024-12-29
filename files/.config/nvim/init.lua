46584402827b866c0210a7d623dd657fc58cf054
moba1 <moba2@protonmail.com>
2024-12-29 11:53:33 +0900

    typescript-language-server の設定をいれた

--- files/.config/nvim/init.lua
+++ files/.config/nvim/init.lua
@@ -18,6 +18,9 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 require'lspconfig'.lua_ls.setup {
   capabilities = capabilities
 }
+require'lspconfig'.ts_ls.setup {
+  capabilities = capabilities
+}
 
 -- autocompletion configuration
 local cmp = require'cmp'
